8d837e32e79e05f026f6b1661650805db9efe42d
김현희 <103232512+hyuneeee1@users.noreply.github.com>
2023-10-24 23:48:19 +0900

    [Bronze IV] Title: 기찍 N, Time: 1064 ms, Memory: 33596 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int arr[] = new int[N];
+		
+			for(int i = arr.length;  i>=1 ; i--) {
+				
+				System.out.println(i);
+				
+			}
+			
+		}
+	}
