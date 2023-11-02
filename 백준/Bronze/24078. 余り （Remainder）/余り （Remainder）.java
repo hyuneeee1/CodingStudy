25fd08429ff76c493bc0c83a84f29195bce8fb4c
김현희 <103232512+hyuneeee1@users.noreply.github.com>
2023-11-02 22:59:56 +0900

    [Bronze V] Title: 余り (Remainder), Time: 220 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {	
+
+public static void main(String[] args) {
+
+			Scanner sc = new Scanner(System.in);
+			
+			int X = sc.nextInt();
+			
+			int a = X % 21;
+			
+			System.out.println(a);
+			
+			}
+    }
