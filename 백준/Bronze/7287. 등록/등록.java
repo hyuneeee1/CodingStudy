9b6952a1f2eb8483fef0ce8b807761efca83a5c1
김현희 <103232512+hyuneeee1@users.noreply.github.com>
2023-11-08 09:46:57 +0900

    [Bronze V] Title: 등록, Time: 116 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    
+		public static void main(String[] args) {
+				
+		String ID = "hyuni028";
+		int grade = 58;
+		
+		System.out.println(grade);
+		System.out.println(ID);
+				
+			}
+}
