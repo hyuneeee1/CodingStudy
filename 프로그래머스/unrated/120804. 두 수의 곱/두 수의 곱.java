37ff2ee739aaa85cb58fb492541cb558aa6a9678
김현희 <103232512+hyuneeee1@users.noreply.github.com>
2023-11-14 14:35:30 +0900

    [level unrated] Title: 두 수의 곱, Time: 0.01 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120804. 두 수의 곱/두 수의 곱.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num1, int num2) {
+
+        int x = num1;
+        int y = num2;
+        
+        int answer = 0;
+        
+        answer = (x * y);
+        return answer;
+    }
+}
