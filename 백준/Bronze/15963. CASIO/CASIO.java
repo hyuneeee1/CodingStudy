40f95f8452f738632f4e2ff4378e3e19c7e1770a
김현희 <103232512+hyuneeee1@users.noreply.github.com>
2023-11-23 09:51:19 +0900

    [Bronze V] Title: CASIO, Time: 216 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+public static void main(String[] args) {
+				
+				Scanner sc = new Scanner(System.in);
+				
+				long N = sc.nextLong();
+				long M = sc.nextLong();
+				
+				if(N == M) {
+					System.out.println(1);
+				}else if(N != M) {
+					System.out.println(0);
+				}
+			}
+    }
