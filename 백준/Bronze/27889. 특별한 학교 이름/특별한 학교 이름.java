875989092e5c70fc83ba9f2c6a6b3ebb34fd427e
김현희 <103232512+hyuneeee1@users.noreply.github.com>
2023-11-27 14:00:12 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 208 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {		
+public static void main(String[] args) {
+				
+			Scanner sc = new Scanner(System.in);
+			
+			String a = sc.nextLine().toUpperCase();
+			
+			switch (a) {
+				case "NLCS" : System.out.println("North London Collegiate School");	
+				break;
+				case "BHA" : System.out.println("Branksome Hall Asia");
+				break;
+				case "KIS" : System.out.println("Korea International School");
+				break;
+				case "SJA" : System.out.println("St. Johnsbury Academy");
+				break;
+			}
+		}
+   }
