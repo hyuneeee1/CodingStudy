ec6c1e710b393f7f7bf373dcf37f1c57507d96ff
김현희 <103232512+hyuneeee1@users.noreply.github.com>
2023-12-04 22:50:30 +0900

    [Bronze V] Title: Zadanie próbne, Time: 208 ms, Memory: 17472 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		String a = sc.nextLine();
+		
+		for(int i = a.length() - 1; i >=0; i--) {
+			System.out.print(a.charAt(i));
+		}
+			
+		}
+
+	}
