b3f80b22a6bd7d49cbdb75561702a16497c96cd8
김현희 <103232512+hyuneeee1@users.noreply.github.com>
2023-12-15 21:13:42 +0900

    [Bronze V] Title: 자동완성, Time: 196 ms, Memory: 17536 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main {
+	
+	  public static void main(String[] args) {
+		  
+	        Scanner sc = new Scanner(System.in);
+	        
+	        String a = sc.next();
+
+
+	    	if(a.equalsIgnoreCase("N")) {
+	    		System.out.println("Naver D2");
+	    	}else {
+	    		System.out.println("Naver Whale");
+	    	}
+
+	  }
+}
