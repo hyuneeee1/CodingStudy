1a4129f11920bf69e647a98554e4c3c8caa725db
김현희 <103232512+hyuneeee1@users.noreply.github.com>
2024-07-18 17:51:29 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 304 ms, Memory: 17952 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -2,19 +2,20 @@ import java.util.Scanner;
 
 public class Main {
 	
-	  public static void main(String[] args) {
-		  
-	        Scanner sc = new Scanner(System.in);
-
-	    		int N = sc.nextInt();
-
-	    		for(int i = 0; i < N; i++) {
-	    			
-	    			for(int k = 0; k < N-i; k++) {
-	    				
-	    				System.out.print("*");
-	    			}
-	    			System.out.println();
-	    		}
-	    	}
+	public static void main(String[] args)  {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for (int i = 1; i <= N; i++) {
+			
+			for (int j = N; j >= i; j--) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
 	}
+	
+	
+}
